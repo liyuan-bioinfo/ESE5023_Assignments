a2308a25ec86d2047a62f4253dc29c4ff1ee8dd6
liyuan-bioinfo <65613804+liyuan-bioinfo@users.noreply.github.com>
2020-10-24 21:40:35 +0800

    change parameter's name

--- assignment3/PS3_1.R
+++ assignment3/PS3_1.R
@@ -13,15 +13,15 @@ setwd("../assignment3/")
 ###################################################
 #           2. build datasets
 ###################################################
-rainfull_unseed<- c(1202.6, 830.1, 372.4, 345.5, 321.2, 244.3, 
+rainfall_unseed<- c(1202.6, 830.1, 372.4, 345.5, 321.2, 244.3, 
                     163.0, 147.8, 95.0, 87.0, 81.2, 68.5, 47.3, 41.1, 
                     36.6, 29.0, 28.6, 26.3, 26.0, 24.4, 21.4, 17.3, 11.5, 
                     4.9, 4.9, 1.0)
-rainfull_seed<-c(2745.6, 1697.1, 1656.4, 978.0, 703.4, 489.1, 
+rainfall_seed<-c(2745.6, 1697.1, 1656.4, 978.0, 703.4, 489.1, 
                  430.0, 334.1, 302.8, 274.7, 274.7, 255.0, 242.5, 200.7, 
                  198.6, 129.6, 119.0, 
                  118.3, 115.3, 92.4, 40.6, 32.7, 31.4, 17.5, 7.7, 4.1)
-rainfull_data_tbl<-as_tibble(data.frame(rainfull=c(rainfull_unseed,rainfull_seed),
+rainfall_data_tbl<-as_tibble(data.frame(rainfall=c(rainfall_unseed,rainfall_seed),
                           seed=(c(rep(0,26),rep(1,26)))
                           )) %>%
   mutate(seed = factor(seed, ordered = TRUE)) 
@@ -30,8 +30,8 @@ rainfull_data_tbl<-as_tibble(data.frame(rainfull=c(rainfull_unseed,rainfull_seed
 #           3. plot two box plot3
 ###################################################
 #assignment3-1.1 Plot two box plots side-by-side of data from the two groups.
-rainfull_data_tbl %>% 
-ggplot( aes(x = seed, y = rainfull, fill = seed)) +
+rainfall_data_tbl %>% 
+ggplot( aes(x = seed, y = rainfall, fill = seed)) +
   geom_boxplot() +
   theme_classic()
 
@@ -39,5 +39,5 @@ ggplot( aes(x = seed, y = rainfull, fill = seed)) +
 #           4. one way anova
 ###################################################
 #assignment3-1.2 Did cloud seeding have an effect on rainfall in this experiment?
-anova_one_way <- aov(rainfull ~ seed, data = rainfull_data_tbl)
+anova_one_way <- aov(rainfall ~ seed, data = rainfall_data_tbl)
 summary(anova_one_way) #no significant
