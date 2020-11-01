bccfcb6eb5d3ba3388adafae02e3887fb1bcb72a
liyuan-bioinfo <65613804+liyuan-bioinfo@users.noreply.github.com>
2020-11-01 18:04:09 +0800

    Add files via upload

---
+++ assignment3/PS3_4.R
@@ -0,0 +1,28 @@
+#author：Liyuan
+#4. Atmospheric Lapse Rate
+
+
+###################################################################
+#           1. init parameters
+###################################################################
+rm(list=ls())
+library("dplyr")
+library("ggplot2")
+library(tidyr)
+
+Elevation<-	c(180,305,381,488,549,640,762,883)
+Temperature<-c(13.3,12.2,13.3,10.0,8.3,9.4,8.3,7.2)
+
+###################################################################
+#   2. fit in lieaer regression and plot the scatter plot
+###################################################################
+#[15 points] Draw a scatter plot with regression line, 
+#and investigate if the lapse rate is 9.8 degrees C km-1.
+
+fit <- lm(Temperature ~ Elevation)
+plot(Temperature ~ Elevation,xlab="Elevation(m)",ylab="Temperature(degrees C)",
+     pch = 20,
+     cex = 2,
+     col = "grey")
+abline(fit, lwd = 5, col = "red")
+summary(fit)$coefficients
