e3d3a469ff843b4d8bc6fd51c721fa0eb681e0f8
liyuan-bioinfo <65613804+liyuan-bioinfo@users.noreply.github.com>
2020-12-16 09:49:41 +0800

    Question2

---
+++ assignment6/Declination_angle.f90
@@ -0,0 +1,16 @@
+module Declination_angle
+!implicit none
+
+contains
+        subroutine cal_dec_angle(N,delta)
+            implicit none
+            real(4)::PI
+            Integer,intent(in)::N
+	    real(4),intent(out)::delta
+            PI=3.1415926
+                !equation2
+                delta= 23.45 * SIN((N+284.)/365 * 360*PI/180)
+
+        end subroutine cal_dec_angle
+
+end module Declination_angle
